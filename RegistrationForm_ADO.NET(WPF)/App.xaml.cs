2377f15f7cdf9ba4ce73fec9e91e22a9fab45808
Admin <Admin@X>
2018-06-28 11:35:54 +0300

    Добавьте файлы проекта.

---
+++ RegistrationForm_ADO.NET(WPF)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RegistrationForm_ADO.NET_WPF_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
